0e88cadf80276da08d756cb1b14c5b7b4b4c88b5
root <atayl16@yahoo.com>
2020-05-07 01:00:50 +0000

    get rid of ugly scrollbar

---
+++ build/precache-manifest.5f7f8d32de8103ad3f71c4664ea8d4eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd66b7067cb275cfa4ba85ec5b155ce",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "c1e95cbd47b578dfedcb",
+    "url": "/robofriends-react/static/css/2.9dc5248e.chunk.css"
+  },
+  {
+    "revision": "d9e65ed39dd84c8852ae",
+    "url": "/robofriends-react/static/css/main.8a53b79e.chunk.css"
+  },
+  {
+    "revision": "c1e95cbd47b578dfedcb",
+    "url": "/robofriends-react/static/js/2.bf34a5fc.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/robofriends-react/static/js/2.bf34a5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e65ed39dd84c8852ae",
+    "url": "/robofriends-react/static/js/main.85b77d51.chunk.js"
+  },
+  {
+    "revision": "ebad7c0ec720d34caa2b",
+    "url": "/robofriends-react/static/js/runtime-main.e48518f2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
